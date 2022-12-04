a0c22014e840258a388107521fd233390fbe9cbc
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2022-12-04 19:19:33 -0300

    feat: day 4 part 2

--- 2022/day4.js
+++ 2022/day4.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync('./day4.input.txt', 'utf-8');
 
 function part1() {
 
-  const { count } = input.split("\n").reduce((acc, line) => {
+  const { count, overlaps } = input.split("\n").reduce((acc, line) => {
     
     const sortedLine = line.split(",").sort((a, b) => {
       const [firstPairStart, firstPairEnd] = a.split("-");
@@ -20,16 +20,21 @@ function part1() {
     const [firstPair, secondPair] = sortedLine;
 
     const firstPairEnd = Number(firstPair.split('-')[1]);
+    const secondPairStart = Number(secondPair.split('-')[0]);
     const secondPairEnd = Number(secondPair.split('-')[1]);
     
     if (firstPairEnd >= secondPairEnd) {
       acc.count += 1;
     }
 
-    return acc;
-  }, { count: 0 })
+    if (firstPairEnd >= secondPairStart) {
+      acc.overlaps += 1;
+    }
 
-  console.log(count);
+    return acc;
+  }, { count: 0, overlaps: 0 })
+
+  console.log(count, overlaps);
 }
 
 part1();
